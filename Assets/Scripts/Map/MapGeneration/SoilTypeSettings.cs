6967d20bd2de6196a6bf39d294dce8fd1aaf20d6
Curtis Bollinger <curtis.bollinger@gmail.com>
2015-11-03 19:03:35 -0600

    About to run code cleanup

---
+++ Assets/Scripts/Map/MapGeneration/SoilTypeSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class SoilTypeSettings
+{
+    [SerializeField]
+    private int seed;
+    public int Seed
+    {
+        get
+        {
+            return seed;
+        }
+
+        set
+        {
+            seed = value;
+        }
+    }
+}
