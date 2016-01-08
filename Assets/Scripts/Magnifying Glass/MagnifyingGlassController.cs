d4fb503ed5d15853f7a80ee6b225f2d9d4c8ef71
Curtis Bollinger <curtis.bollinger@gmail.com>
2016-01-07 19:42:38 -0600

    Magnifying glass working.

---
+++ Assets/Scripts/Magnifying Glass/MagnifyingGlassController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MagnifyingGlassController : MonoBehaviour
+{
+    private void Start()
+    {
+        Cursor.visible = false;
+        StartCoroutine(FollowMouse());
+    }
+
+    private IEnumerator FollowMouse()
+    {
+        while (true)
+        {
+            Vector2 mouseWorldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(mouseWorldPosition.x, mouseWorldPosition.y, -0.5F);
+
+            yield return null;
+        }
+    }
+}
