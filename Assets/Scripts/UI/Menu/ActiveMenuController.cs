dcfd3434e0b2aa56cff202c969709fba578505b4
Curtis Bollinger <curtis.bollinger@gmail.com>
2016-06-26 19:54:45 -0500

    Working on Menu UI

---
+++ Assets/Scripts/UI/Menu/ActiveMenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class ActiveMenuController : MonoBehaviour
+{
+    [SerializeField]
+    private NamedMenuScreen[] menuScreens;
+
+    public void SetCurrentMenuScreen(int index)
+    {
+        NamedMenuScreen menuScreen = null;
+
+        if (index <= menuScreens.Length - 1)
+            menuScreen = menuScreens[index];
+
+        SetCurrentMenuScreen(menuScreen);
+    }
+
+    public void SetCurrentMenuScreen(string title)
+    {
+        SetCurrentMenuScreen(menuScreens.FirstOrDefault(a => a.Title == title));
+    }
+
+    private void SetCurrentMenuScreen(NamedMenuScreen namedMenuScreen)
+    {
+        HideAllMenus();
+        SetNamedMenuScreenActive(namedMenuScreen, true);
+    }
+
+    private void HideAllMenus()
+    {
+        foreach (NamedMenuScreen menuScreen in menuScreens)
+            SetNamedMenuScreenActive(menuScreen, false);
+    }
+
+    private static void SetNamedMenuScreenActive(NamedMenuScreen namedMenuScreen, bool isActive)
+    {
+        if (namedMenuScreen != null && namedMenuScreen.MenuScreen != null)
+            namedMenuScreen.MenuScreen.gameObject.SetActive(isActive);
+    }
+
+    private void Awake()
+    {
+        SetCurrentMenuScreen(0);
+    }
+}
---
+++ Assets/Scripts/UI/Menu/NamedMenuScreen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class NamedMenuScreen
+{
+    [SerializeField]
+    private string title;
+    public string Title
+    {
+        get
+        {
+            return title;
+        }
+    }
+
+    [SerializeField]
+    private CanvasRenderer menuScreen;
+    public CanvasRenderer MenuScreen
+    {
+        get
+        {
+            return menuScreen;
+        }
+    }
+}
